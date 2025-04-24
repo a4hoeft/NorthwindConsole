a7fcc577237d269d4bc60de6201c586f4581fd57
Ana Hoeft <ahoeft7@my.wctc.edu>
2025-04-24 13:39:12 -0500

    [1.15d] Display products for selected category - Enforce eager loading

--- Program.cs
+++ Program.cs
@@ -92,14 +92,13 @@ do
     int id = int.Parse(Console.ReadLine()!);
     Console.Clear();
     logger.Info($"CategoryId {id} selected");
-      Category category = db.Categories.FirstOrDefault(c => c.CategoryId == id)!;
-     Console.WriteLine($"{category.CategoryName} - {category.Description}");
-     foreach (Product p in category.Products)
-     {
-       Console.WriteLine(p.ProductName);
-     }
-     
-     }
+    Category category = db.Categories.Include("Products").FirstOrDefault(c => c.CategoryId == id)!;
+    Console.WriteLine($"{category.CategoryName} - {category.Description}");
+    foreach (Product p in category.Products)
+    {
+      Console.WriteLine($"\t{p.ProductName}");
+    }
+  }
   else if (String.IsNullOrEmpty(choice))
   {
     break;
